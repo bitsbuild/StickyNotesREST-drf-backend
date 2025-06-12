916377b383293b9429f3c8c94bb21ec334f4c6d3
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-12 15:40:12 +0530

    note model added

--- stickynotes/restapi/models.py
+++ stickynotes/restapi/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
-# Create your models here.
+import uuid
+class Note(models.Model):
+    id = models.UUIDField(default=uuid.uuid4,primary_key=True,editable=False)
+    note_title = models.CharField(max_length=70)
+    note_body = models.CharField(max_length=210)
+    note_tags = models.JSONField(default=list)
