e9a07e9c899d366e3113f7a9d164c4bdb3445876
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-12 16:06:08 +0530

    Create Note EndPoint Created

--- stickynotes/restapi/views.py
+++ stickynotes/restapi/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from restapi.serializers import NoteSerializer
+from rest_framework.response import Response
+from rest_framework import status
 class NoteCUD(APIView):
     def post(self,request):
-        pass
+        ser = NoteSerializer(data=request.data)
+        if ser.is_valid():
+            ser.save()
+            return Response({"status":str(ser.data)},status=status.HTTP_201_CREATED)
+        else:
+            return Response({"status":str(ser.errors)},status=status.HTTP_400_BAD_REQUEST)
     def put(self,request,id):
         pass
     def delete(self,request,id):
