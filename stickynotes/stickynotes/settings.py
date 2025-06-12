bd969ed042809a8f10543084e80ce6744bd43a85
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-12 19:45:43 +0530

    Static File Handler So That Deployement Displays Swagger UI Properly

--- stickynotes/stickynotes/settings.py
+++ stickynotes/stickynotes/settings.py
@@ -1,4 +1,9 @@
 from pathlib import Path
+import os
+BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k@--f08jw=dwx939jdi$p9auuwl+w(r3=ipmpcj98xe-sb-)(7'
 DEBUG = True
@@ -15,6 +20,7 @@ INSTALLED_APPS = [
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
