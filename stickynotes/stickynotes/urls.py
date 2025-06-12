d71c42bdcfac8ebe91cb47005747cf96257a4802
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-12 19:38:28 +0530

    Modified URLs To Have Swagger UI as a landing page

--- stickynotes/stickynotes/urls.py
+++ stickynotes/stickynotes/urls.py
@@ -13,7 +13,7 @@ schema_view = get_schema_view(
    permission_classes=(permissions.AllowAny,),
 )
 urlpatterns = [
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('stickynotes/api/',include('restapi.urls'))
 ]
